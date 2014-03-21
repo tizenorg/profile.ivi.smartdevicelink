677ba269853cfa38b0f3769f57b46187b69ee64f
Alexander Kutsan <AKutsan@luxoft.com>
2014-03-21 17:44:29 +0200

    APPLINK-6365 ResumeCtrl: Check images on resume

--- src/components/utils/include/utils/file_system.h
+++ src/components/utils/include/utils/file_system.h
@@ -141,7 +141,7 @@ void Close(std::ofstream* file_stream);
 
 /**
   * @brief Returns full file path
-  *
+  * If filename begins with "/", return unchanged filename
   * @param name file name
   * @return returns full file path.
   */
