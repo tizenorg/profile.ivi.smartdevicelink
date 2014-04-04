c5fd07f49de33655a70ba0d8d572775ec9920d09
Konstantin Kolodiy <KKolodiy@luxoft.com>
2014-04-04 18:36:05 +0400

    APPLINK-6400:
    - Implement global counters: count_of_iap_buffer_full,
                                 count_sync_out_of_memory,
                                 count_of_sync_reboots
    
    Signed-off-by: Justin Dickow <jjdickow@gmail.com>
    
    Conflicts:
    	src/components/application_manager/CMakeLists.txt
    	src/components/application_manager/src/hmi_command_factory.cc
    	src/components/hmi_message_handler/src/messagebroker_adapter.cc

--- src/components/application_manager/src/hmi_command_factory.cc
+++ src/components/application_manager/src/hmi_command_factory.cc
@@ -136,8 +136,8 @@
 #include "application_manager/commands/hmi/tts_set_global_properties_response.h"
 #include "application_manager/commands/hmi/tts_get_capabilities_request.h"
 #include "application_manager/commands/hmi/tts_get_capabilities_response.h"
-#include "application_manager/commands/hmi/vr_perform_interaction_request.h"
-#include "application_manager/commands/hmi/vr_perform_interaction_response.h"
+#include "application_manager/commands/hmi/tts_perform_interaction_request.h"
+#include "application_manager/commands/hmi/tts_perform_interaction_response.h"
 #include "application_manager/commands/hmi/vi_is_ready_request.h"
 #include "application_manager/commands/hmi/vi_is_ready_response.h"
 #include "application_manager/commands/hmi/vi_read_did_request.h"
@@ -238,8 +238,8 @@
 #include "application_manager/commands/hmi/ui_set_display_layout_response.h"
 #include "application_manager/commands/hmi/on_sdl_close_notification.h"
 #include "application_manager/commands/hmi/on_record_start_notification.h"
-#include "application_manager/commands/hmi/basic_communication_system_request.h"
-#include "application_manager/commands/hmi/basic_communication_system_response.h"
+#include "application_manager/commands/hmi/add_statistics_info_notification.h"
+#include "application_manager/commands/hmi/on_system_error_notification.h"
 
 namespace application_manager {
 
@@ -1904,11 +1904,11 @@ CommandSharedPtr HMICommandFactory::CreateCommand(
       }
       break;
     }
-    case hmi_apis::FunctionID::VR_PerformInteraction: {
+    case hmi_apis::FunctionID::TTS_PerformInteraction: {
       if (is_response) {
-        command.reset(new commands::VRPerformInteractionResponse(message));
+        command.reset(new commands::TTSPerformInteractionResponse(message));
       } else {
-        command.reset(new commands::VRPerformInteractionRequest(message));
+        command.reset(new commands::TTSPerformInteractionRequest(message));
       }
       break;
     }
@@ -1940,12 +1940,12 @@ CommandSharedPtr HMICommandFactory::CreateCommand(
       command.reset(new commands::OnRecordStartdNotification(message));
       break;
     }
-    case hmi_apis::FunctionID::BasicCommunication_SystemRequest: {
-      if (is_response) {
-        command.reset(new commands::BasicCommunicationSystemResponse(message));
-      } else {
-        command.reset(new commands::BasicCommunicationSystemRequest(message));
-      }
+    case hmi_apis::FunctionID::SDL_AddStatisticsInfo: {
+      command.reset(new commands::AddStatisticsInfoNotification(message));
+      break;
+    }
+    case hmi_apis::FunctionID::SDL_OnSystemError: {
+      command.reset(new commands::OnSystemErrorNotification(message));
       break;
     }
   }
